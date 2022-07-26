55b6f7958bc8c6ef1371ae7167e9b8435e7b305c
SznAltn <105235609+SznAltn@users.noreply.github.com>
2022-07-26 19:02:23 +0300

    Initial commit

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,56 @@
+﻿using BusinessLayer2.Concrete;
+using BusinessLayer2.ValidationRules;
+using DataAccessLayer2.EntityFramework;
+using EntityLayer2.Concrete2;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi2.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        CategoryManager cm = new CategoryManager(new EfCategoryDal());
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = cm.GetList();
+            return View(categoryvalues);
+        }
+        [HttpGet]
+
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                cm.CategoryAdd(p);
+                return RedirectToAction("GetCategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MvcProjeKampi2
+ 
